764a48682c691eafcef9f51494331d518a381b8c
Tanmoi <52342484+Tanmoi@users.noreply.github.com>
2019-07-04 03:37:59 +0600

    Add files via upload

---
+++ odd_for.php
@@ -0,0 +1,12 @@
+<?php
+$sum = 0;
+for($i=0; $i<= 100; $i++){
+   if
+    ($i%2==1){
+     echo $i. "</br>";
+     $sum = $sum + $i;
+   }
+     
+    }
+   echo "sum = $sum <br>";
+    ?>
---
+++ while_odd.php
@@ -0,0 +1,12 @@
+<?php
+$sum = 0;
+$i=1;
+while ($i<=100){
+    for($i=1;$i<=100;$i++){
+$sum = $sum + $i; 
+echo $i++."<br/>";
+  
+    }
+}
+echo "sum = $sum <br>";
+?>
